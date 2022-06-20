7ea53205b8f75d13e7f3aaa5549f5cdc82870ac7
Costas-Zeimpekis <morpheustudio@gmail.com>
2022-06-20 19:23:43 +0300

    React Hooks 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,28 @@
+import { useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Count from "./Count";
 
 function About() {
+  const refCount = useRef(0);
+  const refParagraph = useRef();
+  const [count, setCount] = useState(0);
   const navigate = useNavigate();
+
+  const value = useMemo(() => {
+    let i;
+    for (i = 0; i <= 10000000; i++) {}
+
+    return count + i;
+  }, [count]);
+
+  function handleRefClick() {
+    refCount.current++;
+  }
+
   return (
     <div>
-      <h1> About</h1>
+      <h1 ref={refParagraph}> About</h1>
+      <Count />
       <button
         onClick={() => {
           // Computing
@@ -14,6 +32,13 @@ function About() {
       >
         Go Home
       </button>
+      <p>Value: {value}</p>
+      <button onClick={() => setCount((prevState) => prevState + 1)}>
+        Add Count
+      </button>
+      <button onClick={handleRefClick}>Add Ref</button>
+      <p>{refCount.current}</p>
+      {console.log(refParagraph.current)}
     </div>
   );
 }
